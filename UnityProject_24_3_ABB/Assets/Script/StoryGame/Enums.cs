00f2b2a8d4c0903b07cb06c67409a2fea67f05e5
JIHyeongJun <wlgudwns6@g.shingu.ac.kr>
2024-05-29 10:19:04 +0900

    Feat: 중간저장

--- UnityProject_24_3_ABB/Assets/Script/StoryGame/Enums.cs
+++ UnityProject_24_3_ABB/Assets/Script/StoryGame/Enums.cs
@@ -24,7 +24,7 @@ public class Enums
     {
         AddExperience,
         GoToNextSotry,
-        GoToRandoemStory
+        GoToRandomStory
 
     }
 
--- UnityProject_24_3_ABB/Assets/Script/StoryGame/GameSystem.cs
+++ UnityProject_24_3_ABB/Assets/Script/StoryGame/GameSystem.cs
@@ -42,19 +42,86 @@ public class GameSystem : MonoBehaviour
     public Stats stats;
     public GAMESTATE currentState;
     public int currentStoryIndex = 1;
-    public StoryModel[] stroyModels; 
+    public StoryModel[] stroyModels;
 
-  
+    public void Start()
+    {
+        ChangeState(GAMESTATE.STORYSHOW);
+    }
+
+
 
 #if UNITY_EDITOR
 
-    
+
     [ContextMenu("Reset Story Models")]
     public void ResetStoryModels()
     {
         stroyModels = Resources.LoadAll<StoryModel>(""); 
     }
 #endif
+
+    public void ChangeState(GAMESTATE temp)
+    {
+        currentState = temp;
+
+        if(currentState == GAMESTATE.STORYSHOW)
+        {
+            StoryShow(currentStoryIndex);
+        }
+    }
+
+    public void ApplyChoice(StoryModel.Result result)
+    {
+        switch(result.resultType)
+        {
+            case StoryModel.Result.ResultType.ChangeHp:
+                stats.currentHpPoint += result.value;
+                ChangeStats(result);
+                break;
+
+            case StoryModel.Result.ResultType.AddExperience:
+                stats.currentXpPoint += result.value;
+                ChangeStats(result);
+                break;
+
+            case StoryModel.Result.ResultType.GoToNextStory:
+                currentStoryIndex = result.value;
+                ChangeState(GAMESTATE.STORYSHOW);
+                ChangeStats(result);
+                break;
+
+            case StoryModel.Result.ResultType.GoTORandomStory:
+                RandomStory();
+                ChangeState(GAMESTATE.STORYSHOW);
+                ChangeStats(result);
+                break;
+            default:
+                Debug.LogError("Unknown type");
+                break;
+
+            
+        }
+    }
+
+    public void ChangeStats(StoryModel.Result result)
+    {
+        if (result.stats.hpPoint > 0) stats.hpPoint += result.stats.hpPoint;
+        if (result.stats.spPoint > 0) stats.spPoint += result.stats.spPoint;
+
+        if (result.stats.currentHpPoint > 0) stats.currentHpPoint += result.stats.currentHpPoint;
+        if (result.stats.currentSpPoint > 0) stats.currentSpPoint += result.stats.currentSpPoint;
+        if (result.stats.currentXpPoint > 0) stats.currentXpPoint += result.stats.currentXpPoint;
+
+        if (result.stats.strength > 0) stats.strength += result.stats.strength;
+        if (result.stats.dexterity > 0) stats.dexterity += result.stats.dexterity;
+        if (result.stats.consitution > 0) stats.consitution += result.stats.consitution;
+        if (result.stats.wisdom > 0) stats.wisdom += result.stats.wisdom;
+        if (result.stats.Intelligence > 0) stats.Intelligence += result.stats.Intelligence;
+        if (result.stats.charisma > 0) stats.charisma += result.stats.charisma;
+
+
+    }
 
     public void StoryShow(int number)
     {
